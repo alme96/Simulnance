927d4115b46e156dfd796c6bf6e8d75ff283cb1c
VincentWuest <44773860+VincentWuest@users.noreply.github.com>
2018-11-12 21:19:07 +0100

    Update Programm_with_trade.py
    I implemented a datacollector and extended the timesteps to multiple days

--- Programm_with_trade.py
+++ Programm_with_trade.py
@@ -9,10 +9,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-
 # Initialize some global variables
 
-m_days = 1
+m_days = 5
 time_steps_per_day = 40
 life_span = 10
 avg_order_waiting_time = 1
@@ -23,13 +22,15 @@ init_shares_a = 1000
 mean = 1
 std = 0.010
 
-def trade_amount(a,b):
-    if a<=b:
-        return random.randint(0,a)
-    else:
-        return random.randint(0,b)
 
-def next_order(): #function that produces time intervall between orders
+def trade_amount(a, b):
+    if a <= b:
+        return random.randint(0, a)
+    else:
+        return random.randint(0, b)
+
+
+def next_order():  # function that produces time intervall between orders
     return math.ceil(np.random.exponential(avg_order_waiting_time, size=1)[0])
 
 
@@ -39,25 +40,42 @@ class TradingModel(Model):
         super().__init__()
         self.limit_order_book = l_o_b
         self.clock = time
-        self.order_arrival =self.clock + next_order() # time when the next order is executed
+        self.order_arrival = self.clock + next_order()  # time when the next order is executed
         self.last_sell = init_stock_price
         self.last_buy = init_stock_price
-        self.schedule = RandomActivation(self) #subclass from mesa with the function "add" and the list of agents schedule.agents
+        self.schedule = RandomActivation(
+            self)  # subclass from mesa with the function "add" and the list of agents schedule.agents
+        self.price_book = []
         for i in range(num_a):
             a = TradingAgent(i, self)
             self.schedule.add(a)
+        self.data_collector_1 = DataCollector(
+            model_reporters={"Limits": TradingModel.get_limit_price}
+        )
+
+    def get_limit_price(self):
+        if len(self.limit_order_book[0]) == 0:
+            get_ask = self.last_sell
+        else:
+            get_ask = min(self.limit_order_book[0])[0]
+        if len(self.limit_order_book[1]) == 0:
+            get_bid = self.last_buy
+        else:
+            get_bid = max(self.limit_order_book[1])[0]
+        return get_ask, get_bid
 
     def refresh_lob(self):
         for sell_tuple in self.limit_order_book[0]:
             if sell_tuple[1] < self.clock:
                 index_t = self.limit_order_book[0].index(sell_tuple)
-                del(self.limit_order_book[0][index_t])
+                del (self.limit_order_book[0][index_t])
         for buy_tuple in self.limit_order_book[1]:
             if buy_tuple[1] < self.clock:
                 index_t = self.limit_order_book[1].index(buy_tuple)
-                del(self.limit_order_book[1][index_t])
+                del (self.limit_order_book[1][index_t])
 
     def step(self):
+        self.data_collector_1.collect(self)
         if self.clock == self.order_arrival:
             self.refresh_lob()
             self.order_arrival = self.clock + next_order()
@@ -69,6 +87,7 @@ class TradingModel(Model):
         for agent in self.schedule.agents:
             if key == agent.unique_id:
                 return agent
+
 
 class TradingAgent(Agent):
 
@@ -82,18 +101,18 @@ class TradingAgent(Agent):
         if coin < 0.5:
             self.buy_order()
         else:
-            self.sell_order()        
+            self.sell_order()
 
     def sell_order(self):
         if len(self.model.limit_order_book[0]) == 0:
             ask_price = self.model.last_sell
         else:
             (ask_price, a_deadline, a_id) = min(self.model.limit_order_book[0])
-        s_order = round(ask_price * (np.random.normal(mean, std, None))) #Offer creation
+        s_order = round(ask_price * (np.random.normal(mean, std, None)))  # Offer creation
         if len(self.model.limit_order_book[1]) == 0:
             order_deadline = self.model.clock + life_span
             self.model.limit_order_book[0].append((s_order, order_deadline, self.unique_id))
-            print("No trade at time",self.model.clock,":")
+            print("No trade at time", self.model.clock, ":")
             print(self.model.limit_order_book)
             print()
             return
@@ -101,38 +120,42 @@ class TradingAgent(Agent):
         if s_order > bid_price:
             order_deadline = self.model.clock + life_span
             self.model.limit_order_book[0].append((s_order, order_deadline, self.unique_id))
-            print("No trade at time",self.model.clock,":")
+            print("No trade at time", self.model.clock, ":")
             print(self.model.limit_order_book)
             print()
         else:
             self.model.last_sell = bid_price
+            self.model.price_book.append(bid_price)
             buyer = self.model.trading_partner(b_id)
-            N_max_sell=self.shares
-            N_max_buy=int(buyer.cash/bid_price)
-            N_trade=trade_amount(N_max_sell,N_max_buy)
-            self.cash=self.cash+bid_price*N_trade
-            self.shares=self.shares-N_trade
-            buyer.cash=buyer.cash-bid_price*N_trade
-            buyer.shares=buyer.shares+N_trade
-            print("Trade at time",self.model.clock,":")
-            print("Seller", self.unique_id, "meets Buyer", buyer.unique_id, "and sells",N_trade,"shares at",bid_price,"each.")
-            print("Agent",self.unique_id," now has",self.cash," amount of cash and ",self.shares,"amount of shares.")
-            print("Agent",buyer.unique_id,"now has",buyer.cash,"amount of cash and",buyer.shares,"amount of shares.")
+            N_max_sell = self.shares
+            N_max_buy = int(buyer.cash / bid_price)
+            N_trade = trade_amount(N_max_sell, N_max_buy)
+            self.cash = self.cash + bid_price * N_trade
+            self.shares = self.shares - N_trade
+            buyer.cash = buyer.cash - bid_price * N_trade
+            buyer.shares = buyer.shares + N_trade
+            print("Trade at time", self.model.clock, ":")
+            print("Seller", self.unique_id, "meets Buyer", buyer.unique_id, "and sells", N_trade, "shares at",
+                  bid_price, "each.")
+            print("Agent", self.unique_id, " now has", self.cash, " amount of cash and ", self.shares,
+                  "amount of shares.")
+            print("Agent", buyer.unique_id, "now has", buyer.cash, "amount of cash and", buyer.shares,
+                  "amount of shares.")
             print(self.model.limit_order_book)
             print()
             index_t = self.model.limit_order_book[1].index((bid_price, b_deadline, b_id))
-            del(self.model.limit_order_book[1][index_t])
+            del (self.model.limit_order_book[1][index_t])
 
     def buy_order(self):
         if len(self.model.limit_order_book[1]) == 0:
-            bid_price = self.model.last_buy 
+            bid_price = self.model.last_buy
         else:
-            (bid_price, b_deadline, b_id) = max(self.model.limit_order_book[1]) 
-        b_order = round(bid_price * (np.random.normal(mean, std, None))) #Offer creation
+            (bid_price, b_deadline, b_id) = max(self.model.limit_order_book[1])
+        b_order = round(bid_price * (np.random.normal(mean, std, None)))  # Offer creation
         if len(self.model.limit_order_book[0]) == 0:
             order_deadline = self.model.clock + life_span
             self.model.limit_order_book[1].append((b_order, order_deadline, self.unique_id))
-            print("No trade at time",self.model.clock,":")
+            print("No trade at time", self.model.clock, ":")
             print(self.model.limit_order_book)
             print()
             return
@@ -140,27 +163,30 @@ class TradingAgent(Agent):
         if b_order < ask_price:
             order_deadline = self.model.clock + life_span
             self.model.limit_order_book[1].append((b_order, order_deadline, self.unique_id))
-            print("No trade at time",self.model.clock,":")
+            print("No trade at time", self.model.clock, ":")
             print(self.model.limit_order_book)
             print()
         else:
             self.model.last_buy = ask_price
+            self.model.price_book.append(ask_price)
             seller = self.model.trading_partner(a_id)
-            N_max_buy=int(self.cash/ask_price)
-            N_max_sell=seller.shares
-            N_trade=trade_amount(N_max_sell,N_max_buy)
-            seller.cash=self.cash+ask_price*N_trade
-            seller.shares=self.shares-N_trade
-            self.cash=self.cash-ask_price*N_trade
-            self.shares=self.shares+N_trade
-            print("Trade at time",self.model.clock,":")
-            print("Buyer", self.unique_id, "meets Seller", seller.unique_id,"and buys",N_trade,"shares at",ask_price,"each.")
-            print("Agent",self.unique_id,"now has",self.cash,"amount of cash and",self.shares,"amount of shares.")
-            print("Agent",seller.unique_id,"now has",seller.cash,"amount of cash and",seller.shares,"amount of shares.")
+            N_max_buy = int(self.cash / ask_price)
+            N_max_sell = seller.shares
+            N_trade = trade_amount(N_max_sell, N_max_buy)
+            seller.cash = self.cash + ask_price * N_trade
+            seller.shares = self.shares - N_trade
+            self.cash = self.cash - ask_price * N_trade
+            self.shares = self.shares + N_trade
+            print("Trade at time", self.model.clock, ":")
+            print("Buyer", self.unique_id, "meets Seller", seller.unique_id, "and buys", N_trade, "shares at",
+                  ask_price, "each.")
+            print("Agent", self.unique_id, "now has", self.cash, "amount of cash and", self.shares, "amount of shares.")
+            print("Agent", seller.unique_id, "now has", seller.cash, "amount of cash and", seller.shares,
+                  "amount of shares.")
             print(self.model.limit_order_book)
             print()
             index_t = self.model.limit_order_book[0].index((ask_price, a_deadline, a_id))
-            del(self.model.limit_order_book[0][index_t])
+            del (self.model.limit_order_book[0][index_t])
 
 
 # The limit_order_book is a list with 2 entries, which again are lists. The first one stores the sell_order's
@@ -171,13 +197,28 @@ class TradingAgent(Agent):
 #               [(0, time_steps_per_day * m_days), (init_stock_price, life_span)]]
 init_l_o_b = [[], []]
 first_try = TradingModel(init_l_o_b, 0)
-for j in range(time_steps_per_day):
-    first_try.step()
-print()
-print("Final limit order book:")
-print(first_try.limit_order_book)
+# for j in range(time_steps_per_day):
+#     first_try.step()
+# print()
+# print("Final limit order book:")
+# print(first_try.limit_order_book)
 
-    
 # Yet missing: - The transaction of money and shares
 #              - Generalization to daily sections
 #              - A Data collector
+
+# Going from a single day to multiple:
+#
+for day in range(m_days):
+    for j in range(time_steps_per_day):
+        first_try.step()
+    print()
+    print("Limit Order Book after ", day + 1, " days:")
+    print(first_try.limit_order_book, "\n")
+    first_try.limit_order_book = [[], []]
+
+limits = first_try.data_collector_1.get_model_vars_dataframe()
+print("The ask_price and the bid_price at every step:")
+print(limits, "\n")
+print("The history of price returns:")
+print(first_try.price_book)
